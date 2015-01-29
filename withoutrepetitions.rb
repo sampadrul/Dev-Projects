a42f05a81cc2aa1a00233bd44d569544e9c1fd3b
Sam Padrul <sam.padrul@gmail.com>
2015-01-29 14:46:53 -0600

    Delete withoutrepetitions.rb

--- withoutrepetitions.rb
+++
@@ -1,9 +0,0 @@
-file = ARGV[0]
-
-File.open(file).each { |line|
-
-	text = line.split(" ")
-	words = text.each { |word| word.split() }
-	words.map! { |word| word.to_s.squeeze }	
-	puts words.join(" ")
-}
