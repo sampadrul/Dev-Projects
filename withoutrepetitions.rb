91a89f621111e22aa243153c37e8be7cf69bc055
sampadrul <sam.padrul@gmail.com>
2015-01-29 14:45:15 -0600

    withoutrepetitions update

--- withoutrepetitions.rb
+++ withoutrepetitions.rb
@@ -3,8 +3,7 @@ file = ARGV[0]
 File.open(file).each { |line|
 
 	text = line.split(" ")
-	
-	words = text.map { |word| word.split() }
-	
-	print words.to_s.squeeze.join()
+	words = text.each { |word| word.split() }
+	words.map! { |word| word.to_s.squeeze }	
+	puts words.join(" ")
 }
