8a23988ecb9e3a66d48a5e436019ab4a9bb2f76b
sampadrul <sam.padrul@gmail.com>
2015-01-29 15:37:42 -0600

    updated withoutrepetitions.rb

--- Code-Eval/withoutrepetitions.rb
+++ Code-Eval/withoutrepetitions.rb
@@ -1,9 +1,10 @@
 file = ARGV[0]
 
 File.open(file).each { |line|
-
-	text = line.split(" ")
-	words = text.each { |word| word.split() }
-	words.map! { |word| word.to_s.squeeze }	
-	puts words.join(" ")
+	if line.length > 0
+		text = line.split(" ").each { |word| word.split() }
+		text.map! { |word| word.to_s.squeeze }	
+		puts text.join(" ")
+		
+	end
 }
