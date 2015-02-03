7d1e874bc81996fee4b4833db46e16d5ba2c0362
sampadrul <sam.padrul@gmail.com>
2015-02-03 08:26:33 -0600

    refactored fizzbuzz to less lines and changed some methods

--- Code-Eval/fizzbuzz.rb
+++ Code-Eval/fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzBuzz(x, y, n)
 	line = ""
-	(1..n).each do |i|
+	1.upto(n) do |i|
 		case 
 		when i % x == 0 && i % y == 0 
 			line << "FB "
@@ -19,7 +19,5 @@ file = ARGV[0]
 
 File.open(file).each do |line|
 	x, y, n = line.split(" ").map(&:to_i)
-	if x.between?(1,20) && x.between?(1,20) && n.between?(21,100)
-		puts fizzBuzz(x, y, n)
-	end
+	puts fizzBuzz(x, y, n) if x.between?(1,20) && x.between?(1,20) && n.between?(21,100)
 end
