77ae315632ce57099606b0a7f036a708630e26e6
sampadrul <sam.padrul@gmail.com>
2015-02-10 15:30:29 -0600

    updated line split method to map to array using to_i

--- Code-Eval/easy/multiplesofnumber.rb
+++ Code-Eval/easy/multiplesofnumber.rb
@@ -1,10 +1,8 @@
 file = ARGV[0]
 
 File.open(file).each do |line|
-	x, n = line.split(",")
-	x = x.to_i
-	n = n.to_i
-	i = 3
+	x, n = line.split(",").map { |x| x = x.to_i }
+	i = 1
 	t = 0
 	
 	until t >= x do
