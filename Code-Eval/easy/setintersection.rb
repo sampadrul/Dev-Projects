48174322d09511b4adf5482fde603959a80fdb72
sampadrul <sam.padrul@gmail.com>
2015-02-18 13:29:24 -0600

    update to set intersection solution

--- Code-Eval/easy/setintersection.rb
+++ Code-Eval/easy/setintersection.rb
@@ -1,9 +1,13 @@
+class Intersector
+	def self.intersector(line)
+		first_set, second_set = line.split(";").map { |nums| nums.split(",").map(&:to_i) }
+		puts (first_set & second_set).count > 0 ? (first_set & second_set).join(",") : ""
+	end
+end
+	
+
 file = ARGV[0]
 
 File.open(file).each do |line|
-
-	first_set, second_set = line.split(";").map { |nums| nums.split(",").map(&:to_i) }
-
-	puts (first_set & second_set).count > 0 ? (first_set & second_set).join(",") : ""
-	
+	Intersector.intersector(line)
 end
