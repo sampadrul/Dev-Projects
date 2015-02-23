fb2ec00972b29d144a6937429eee2c783791ce5b
Sam Padrul <sam@Sam-Padruls-MacBook-Pro.local>
2015-02-22 19:11:13 -0600

    updated stockpicker

--- Stock Picker/stock_picker.rb
+++ Stock Picker/stock_picker.rb
@@ -1,15 +1,10 @@
 def stock_picker(prices)
-	buy = nil
-	sell = nil
-	profit = 0
-	
-	prices[0..(prices.length - 1)].each do |buy|
-		
-		prices[buy + 1..prices.length].each do |sell|
-			diff = prices[buy].to_i - prices[sell].to_i
+	prices[0..(prices.length - 2)].each do |buy|
+		prices[(buy + 1)..(prices.length)].each do |sell|
+			print sell - buy			
 		end
-	end
-end
+	end	
 
-prices = [3,5,2,6,9,3,3,7,10]
-puts stock_picker(prices)
+
+end
+stock_picker([17,3,6,9,15,8,6,1,10])
