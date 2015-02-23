80ed8d90ad60f4d6fadf1efa1ae58226353c5cf4
sampadrul <sam.padrul@gmail.com>
2015-02-23 09:33:14 -0600

    added stock_picker solution for odin project

--- Stock Picker/stock_picker.rb
+++ Stock Picker/stock_picker.rb
@@ -1,10 +1,25 @@
 def stock_picker(prices)
-	prices[0..(prices.length - 2)].each do |buy|
-		prices[(buy + 1)..(prices.length)].each do |sell|
-			print sell - buy			
+	
+	profit = 0
+	buy_day = nil
+	sell_day = nil
+	
+	prices[0..(prices.length - 2)].each_with_index do |buy, index|
+		prices[(index + 1)..prices.length].each do |sell|
+			
+			
+			difference = sell - buy
+			
+			if difference > profit
+				profit = difference
+				buy_day = buy
+				sell_day = sell
+			end		
 		end
 	end	
-
-
+	
+	puts "buy on day #{buy_day}, and sell on day #{sell_day} for a profit of #{profit}"
+		
 end
+
 stock_picker([17,3,6,9,15,8,6,1,10])
